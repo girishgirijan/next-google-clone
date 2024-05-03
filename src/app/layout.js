6644fb596da691e26452aa2368ffc5b138a8a0ed
Girish Girijan <girishvazheliparambil@gmail.com>
2024-05-03 14:19:23 -0300

    Completed Home header

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Google Clone",
-  description: "An open source Google clone",
+  description: "An open source Google clone ",
 };
 
 export default function RootLayout({ children }) {
