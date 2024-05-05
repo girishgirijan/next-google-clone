f9bcbc1f0e7691e222da841a6e1a074c3362316b
Girish Girijan <girishvazheliparambil@gmail.com>
2024-05-05 14:29:53 -0300

    Added Error handling page in Search

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import Link from "next/link";
 
-export default function WebSearchPage() {
+export default async function WebSearchPage({ searchParams }) {
+  const res = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_API_KEY}&cx=${process.env.NEXT_CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+
+  if (!res.ok) throw new Error("Something went wrong!");
+  const data = await res.json();
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-2xl mb-4">
+          No results found for <b>{searchParams.searchTerm}</b>
+        </h1>
+        <p className="text-lg">
+          Try searching the web or images for something else.
+          <Link href="/" className="text-blue-500"> Home</Link>
+        </p>
+      </div>
+    );
+  }
   return (
-    <div className='text-red-600'>
-      Web search page
-    </div>
-  )
+    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
+  );
 }
