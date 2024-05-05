58f734c9ba10cc7e244d4f7e94833fa958264c13
Girish Girijan <girishvazheliparambil@gmail.com>
2024-05-05 16:26:38 -0300

    Implemented Pagination

--- src/components/ImageSearchResults.jsx
+++ src/components/ImageSearchResults.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import PaginationButtons from "./PaginationButtons";
 
 export default function ImageSearchResults({ results }) {
   return (
@@ -28,6 +29,9 @@ export default function ImageSearchResults({ results }) {
             </div>
           </div>
         ))}
+      </div>
+      <div className="ml-16">
+        <PaginationButtons />
       </div>
     </div>
   );
